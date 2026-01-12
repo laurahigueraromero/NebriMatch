768d892479e25e4fd0651bf4a631d2551accd8f0
Laura Higuera <laurahigueraromero@gmail.com>
2026-01-12 08:53:00 +0100

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,37 @@
 
 import './App.css'
+//TODO: importar import { RouterProvider, createBrowserRouter } from "react-router-dom"; y las paginas
+
+
+const router = createBrowserRouter([
+	{
+		path: "/", //Ruta de la URL
+    //errorElement: <ErrorPage />, //Componente para cargar página de errores
+		children: [
+      {
+        path: '/',
+        element: <Login/>
+      },
+      {
+        path: '/users',
+        element: <Users />,
+        loader: async () => {
+          const res = await fetch("https://jsonplaceholder.typicode.com/users");
+          return res.json(); //loader para manejar petición de datos
+        }
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/products',
+        element: <Products />
+      },
+    ], //Componentes que son navegables
+	},
+]);
+
 
 function App() {
 
