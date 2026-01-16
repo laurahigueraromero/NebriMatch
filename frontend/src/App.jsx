275b49648b708e63d607629b27e98b081b99fca2
Laura Higuera <laurahigueraromero@gmail.com>
2026-01-16 08:42:22 +0100

    correcion-errores

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,7 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import './App.css'
-//TODO: importar import { RouterProvider, createBrowserRouter } from "react-router-dom"; y las paginas
+import AppRouter from './router/AppRouter';
 
-
-/*const router = createBrowserRouter([
-	{
-		path: "/", //Ruta de la URL
-    //errorElement: <ErrorPage />, //Componente para cargar página de errores
-		children: [
-      {
-        path: '/',
-        element: <Login/>
-      },
-      {
-        path: '/users',
-        element: <Users />,
-        loader: async () => {
-          const res = await fetch("https://jsonplaceholder.typicode.com/users");
-          return res.json(); //loader para manejar petición de datos
-        }
-      },
-      {
-        path: '/about',
-        element: <About />
-      },
-      {
-        path: '/products',
-        element: <Products />
-      },
-    ], //Componentes que son navegables
-	},
-]);*/
 
 
 function App() {
@@ -38,7 +9,7 @@ function App() {
 
   return (
     <>
-     
+     <AppRouter/>
     </>
   )
 }
